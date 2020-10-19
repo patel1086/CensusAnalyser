29bb02d01a51ce9b62fed41b447674e090a0ea8e
Jitendra Patel <41681183+ammipatel@users.noreply.github.com>
2020-10-19 23:41:01 +0530

    [Jitendra]Added: Method to load IndianCensusCSV file

---
+++ src/main/java/censusanalyser/IndiaStateCodeCSV.java
@@ -0,0 +1,21 @@
+package censusanalyser;
+
+import com.opencsv.bean.CsvBindByName;
+
+public class IndiaStateCodeCSV {
+	@CsvBindByName(column = "SrNo", required = true)
+	public String srNo;
+	@CsvBindByName(column = "State Name", required = true)
+	public String stateName;
+	@CsvBindByName(column = "TIN", required = true)
+	public int tin;
+	@CsvBindByName(column = "StateCode", required = true)
+	public String stateCode;
+
+	@Override
+	public String toString() {
+		return "IndiaStateCSV{" + "srNo='" + srNo + '\'' + ", stateName='" + stateName + '\'' + ", tin=" + tin
+				+ ", stateCode='" + stateCode + '\'' + '}';
+	}
+
+}
